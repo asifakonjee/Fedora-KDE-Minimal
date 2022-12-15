c893b7f6b990321ec67bd5202f4ba9a3e59ca1dd
Dr. Asifur Rahman Akonjee <asifakonjee@hotmail.com>
2022-12-15 19:48:58 +0000

    Update fedora-kde-min-packages.sh

--- fedora-kde-min-packages.sh
+++ fedora-kde-min-packages.sh
@@ -7,7 +7,7 @@ fi
 
 # RPM Fusion & Others:
   echo "RPM Fusion..."
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Update
 sudo dnf update
